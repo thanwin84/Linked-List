387b5b92df9bb2dc16a88e6151f93bb9756b994e
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-02-06 16:42:01 +0600

    Create remove duplicates from linked list 2.cpp

---
+++ remove duplicates from linked list 2.cpp
@@ -0,0 +1,38 @@
+//time: O(n) and space: O(n)
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        ListNode* dummy = new ListNode(-1);
+        ListNode* prev = dummy;
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode* deleteNode = new ListNode(-250);
+        ListNode* current = head;
+        while (current && current->next) {
+            if (current->val == current->next->val || current->val == deleteNode->val) {
+                deleteNode = current;
+            }
+            else {
+                prev->next = current;
+                prev = current;
+                
+            }
+            current = current->next;
+        }
+        if (current && current->val != deleteNode->val) {
+            prev->next = current;
+        }
+      //if we dont' get last node
+      // we have to cut prev node so that it doesn't point to nullptr
+        else prev->next = nullptr;
+        return dummy->next;
+    }
+    void show(ListNode* head) {
+        ListNode* current = head;
+        while (current) {
+            cout << current->val << " ";
+            current = current->next;
+        }
+    }
+};
