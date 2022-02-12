5fc821a2087add6ec157c4dee492348ac4f68d74
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-02-12 20:25:25 +0600

    Create intersection node in two linked llist.cpp

---
+++ intersection node in two linked llist.cpp
@@ -0,0 +1,3 @@
+//naive approach:
+//we will run two while loops. each node address in outer loop will be checked in inner loop if the both address is same. This is will take O(n * m) time.
+//using hashmap: we will store each node either of two loop. then we will treverse other link list to check if there is any same node. time: O(n + m) and space: O(n)
