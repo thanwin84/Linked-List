ba21bb57475bf29a4b127e78f0567316f2abb346
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-06 01:21:44 +0600

    Update Nth node from end of linked list.cpp

--- Nth node from end of linked list.cpp
+++ Nth node from end of linked list.cpp
@@ -28,3 +28,28 @@ int getNthFromLast(Node *head, int n)
        return -1;
        
 }
+// Optimized approach
+// algorithm: take two pointers(first_pointer and second_pointer)
+// treverse n nodes with first pointer(we only have to traverse n -1 times to reach at nth position).
+// then treverse first pointer and second pointer simulteniously until the first_pointer.next is not null. 
+// When second pointer.next becomes null, second pointer will reach at the desire position
+// time: O(n) and space: O(1)
+// in the worst case we have to traverse the linked list only once
+int getNthFromLast(Node *head, int n)
+{
+       // Your code here
+       Node * first = head;
+       Node * second = head;
+       for (int i = 0; i < n - 1; i++){
+           first = first->next;
+           if (first == nullptr){
+               return -1;
+           }
+       }
+       while (first->next){
+           first = first->next;
+           second = second->next;
+       }
+       return second->data;
+       
+}
