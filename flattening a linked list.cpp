52a3750f6634ee9b9cf7611fd10498713e1014f1
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-05 12:51:44 +0600

    Update flattening a linked list.cpp

--- flattening a linked list.cpp
+++ flattening a linked list.cpp
@@ -1,1 +1,35 @@
 
+Node* merge_linked_list(Node* A, Node* B) {
+    Node* temp = new Node(0);
+    Node* head = temp;
+    while (A && B) {
+        if (A->data < B->data) {
+            temp->bottom = A;
+            temp = temp->bottom;
+            A = A->bottom;
+        }
+        else {
+            temp->bottom = B;
+            temp = temp->bottom;
+            B = B->bottom;
+        }
+    }
+    if (A) {
+        temp->bottom = A;
+    }
+    else if (B) {
+        temp->bottom = B;
+    }
+    return head->bottom;
+}
+
+Node* flatten(Node* root)
+{
+    // Your code here
+    if (root == nullptr || root->next == nullptr) {
+        return root;
+    }
+    root->next = flatten(root->next);
+    Node* new_node = merge_linked_list(root, root->next);
+    return new_node;
+}
