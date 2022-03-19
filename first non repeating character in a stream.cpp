cb930ff89ec18e0072ad41982f4015bb5c0f4454
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-19 21:21:01 +0600

    Create first non repeating character in a stream.cpp

---
+++ first non repeating character in a stream.cpp
@@ -0,0 +1,26 @@
+// time: O(n) and space: O(26)
+class Solution {
+public:
+	string FirstNonRepeating(string A) {
+		// Code here
+		unordered_map<char, int> seen;
+		string result;
+		queue<char> q;
+		for (auto i : A) {
+			q.push(i);
+			if (seen.count(i)) seen[i] += 1;
+			else seen[i] = 1;
+			while (q.size() > 0 && seen[q.front()] > 1) {
+				q.pop();
+			}
+			if (q.size() > 0) {
+				result += q.front();
+			}
+			else {
+				result += '#';
+			}
+		}
+		return result;
+	}
+
+};
