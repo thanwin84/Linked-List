829f17d93591e8af85c219d7c1ef62eb4b2398c5
Than Win Hline <60491531+thanwin84@users.noreply.github.com>
2022-03-19 22:54:25 +0600

    Update first non repeating character in a stream.cpp

--- first non repeating character in a stream.cpp
+++ first non repeating character in a stream.cpp
@@ -1,4 +1,8 @@
 // time: O(n) and space: O(26)
+// We could solve the problem simply by using hashmap but here we have cotinuous flow of character which is coming. so we can't just use only hashmap here
+// here we have to find out the first non repeating character every time a char is inserted. So to maintain this order we can use queue. 
+// whenever we get a char, we will push it to the q and hashmap. we will check if the peak of the queue freq is more than 1, that means it's not firstNonRepeatingCha
+//if freq is 1, that means it's the first non repeating char
 
 class Solution {
 public:
